41e52213f6da262b9a58599c62773133312cab4c
Kuan Pang <62387204+Kuan-Pang@users.noreply.github.com>
2021-10-17 08:02:39 -0400

    Update Exceptions.java

--- src/main/java/com/courseApp/constants/Exceptions.java
+++ src/main/java/com/courseApp/constants/Exceptions.java
@@ -7,7 +7,6 @@ public class Exceptions {
     public static final String NO_EXISTING_SCHEDULE = "There is no possible schedule for these courses.";
     public static final String COMMAND_NOT_FOUND = "No such command.";
 
-    public static final String NO_SECTION_CODE = "The section code is not provided ControlPresentInfo the course code.";
     public static final String NO_EXISTING_SCHEDULE = "There is no possible schedule for these courses.";
 
 }
