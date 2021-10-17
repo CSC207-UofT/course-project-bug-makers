eaa7ec6279fde06efbf0cb0d6bf06d4fb54aead2
Kuan Pang <62387204+Kuan-Pang@users.noreply.github.com>
2021-10-17 08:04:30 -0400

    Update Exceptions.java

--- src/main/java/com/courseApp/constants/Exceptions.java
+++ src/main/java/com/courseApp/constants/Exceptions.java
@@ -7,6 +7,4 @@ public class Exceptions {
     public static final String NO_EXISTING_SCHEDULE = "There is no possible schedule for these courses.";
     public static final String COMMAND_NOT_FOUND = "No such command.";
 
-    public static final String NO_EXISTING_SCHEDULE = "There is no possible schedule for these courses.";
-
 }
