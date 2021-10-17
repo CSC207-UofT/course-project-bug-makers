48c30a29160dbb2d2d1faed5a9e9c41c74ce7d61
Kuan-Pang <62387204+Kuan-Pang@users.noreply.github.com>
2021-10-17 12:46:53 -0400

    CMD logo added.

--- src/main/java/com/courseApp/constants/Constants.java
+++ src/main/java/com/courseApp/constants/Constants.java
@@ -199,4 +199,26 @@ public class Constants {
     }
 
 
+    //// Driver Constants
+
+    public static final String WELCOME_LOGO = """
+                                                                                                                                                                                                                   \s
+                                                                   `-/+osso+:-                                                                                .oooo+`                                              \s
+                                                                 `/ydddhhhhddo                                                                               `sddddd+                                              \s
+                        .::- .:///:.  .:///-`   ::::`     -:::` `sdddo-.``.-/`     .-////:.      -:::`    .:::.   -::. `-//-   .:////:-.     .:////-`        /ddh:ddd-      .::- .:///-`    `::: `-///:.           \s
+                        +ddhohhhdddhoshhhdddh:  +ddd+    -dddy` +dddo            .shdddhddhs:    oddd.    /ddd/   odds/hddd/ .shddhhhdd+   :yhdhyyddho`     -ddd/ oddy`     /dddohhhdddy/   -dddoyhhdddh+`         \s
+                        +dddh:..:hdddy:../dddh  `sddd-  `yddh.  yddd.           .hddh:..-sddd/   oddd.    /ddd/   odddds/::. /dddo.`.-:`  :dddo.``-ydds    `yddy  `hddo     /dddh:..-yddd-  -dddd/..-sddd+         \s
+                        +ddd+    sddd:    hddd`  `yddy` +ddd:   hddd.           +ddd/    `dddh   oddd.    /ddd/   oddd+      .ydddhs+-`   ydddsooooyddd`   +ddd/...+ddd:    /ddd+    :dddo  -ddds    `dddh         \s
+                        +ddd:    oddd-    yddd`   -ddd+.dddo    oddd/           oddd:     hddd`  oddd-    +ddd/   oddd-       `-+shdddh+  hdddsssssssss`  -ddddddddddddh.   /ddd/    -ddds  -ddds    `dddh         \s
+                        +ddd:    oddd.    yddd`    /dddsddy`    .hddh/.` ```.`  -ddds`  `:ddds   oddd/   .yddd/   oddd-      ``   `-hddd. +ddd/`     ``  `hddh++++++oddds   /dddy`  `oddd/  -dddh.  `/ddds         \s
+                        +ddd:    oddd.    yddd`     +ddddh.      -sdddhysyyhd/   /hddhssyddds`   -hdddssshhddd/   oddd-      +hso++ohdds` `ohddyo+oosy:  oddd/       oddd/  /dddhhssydddo`  -ddddhssydddy.         \s
+                        :sss-    +sss.    osss`     `yddd/         -/syyyyys+.    `/oyyyys+-      .+syyys/`+ss:   /sss.      -osyyyyso:`    ./oyyyyys+. .ssso`       `ssss` /ddd//syyyo:    -dddo:syyys:`          \s
+                                                   `:dddo                                                                                                                   /ddd:           -dddo                  \s
+                                                -ssydddo                                                                                                                    /ddd:           -dddo                  \s
+                                                :yhys+-                                                                                                                     /hhh:           -hhh+                  \s
+                                                                                                                                                                                                                   \s
+                                                                                                                                                                                                             \s
+            """;
+
+
 }
