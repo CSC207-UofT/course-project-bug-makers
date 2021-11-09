4004adb43447576ff47ed7a6b5b3dc9f40653c12
Kuan-Pang <62387204+Kuan-Pang@users.noreply.github.com>
2021-11-09 17:27:14 -0500

    `CourseService` refactored.

--- src/main/java/com/courseApp/courseService/ControlPresentInformation.java
+++ src/main/java/com/courseApp/courseService/ControlPresentInformation.java
@@ -8,10 +8,30 @@ import com.courseApp.entity.Schedule;
  */
 public interface ControlPresentInformation {
 
+    /**
+     * Return string of course information, including title, description, prerequisite and sections.
+     *
+     * @param courseCode course code
+     * @return course information String
+     * @throws Throwable exception
+     */
     String getCourseGeneralInformation(String courseCode) throws Throwable;
 
+    /**
+     * Return string of section schedule
+     *
+     * @param courseCodeWSection course code
+     * @return section information String
+     * @throws Throwable exception
+     */
     String getSectionInformation(String courseCodeWSection) throws Throwable;
 
+    /**
+     * Return the String representation of schedule.
+     *
+     * @param schedule Schedule obj
+     * @return String representation of schedule obj
+     */
     String getScheduleSummary(Schedule schedule);
 
 }
