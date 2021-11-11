f38a121cbd524e19daffe8be9ce94c320d82533b
Lola-W <79666594+Lola-W@users.noreply.github.com>
2021-11-11 00:28:17 -0500

    Upgraded `CalendarFactory` with 6 new types of calendars to choose. Updated `CalendarService` documentation to illustrate the usage.

--- src/main/java/com/courseApp/calendarService/CalendarFactory.java
+++ src/main/java/com/courseApp/calendarService/CalendarFactory.java
@@ -1,26 +1,79 @@
 package com.courseApp.calendarService;
 
 
+import com.courseApp.constants.Constants;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
 /**
  * CalendarFactory for generating calendar, which follows the factor design pattern.
  */
-public class CalendarFactory implements UsePresentable{
+abstract class CalendarFactory implements UsePresentable{
     protected final Map<String, Map<String, ArrayList<String>>> formattedSchedule;
     public CalendarFactory(Map<String, Map<String, ArrayList<String>>> formattedSchedule) {
         this.formattedSchedule = formattedSchedule;
     }
 
     /**
-     * Should never be called.
+     * Convert the String weekday to an integer format indicating its location in any Calendar.
      *
-     * @return String of Null.
+     * @param weekday String of Days of week
      */
-    @Override
-    public String present() {
-        return null;
+    abstract int getWeekDay(String weekday);
+
+    /**
+     * Reformat the list to modify the cell size according to the length of the event.
+     *
+     * @param list the Calendar list
+     * @param weekDay weekday's location in the Calendar
+     * @param startingT Starting time of this section
+     * @param timeDifference the duration of this event in hour(s)
+     */
+    protected void resizeCell(List<String> list, int weekDay, int startingT, int timeDifference){
+        // modify the cell if the duration is longer than an hour
+        while (timeDifference > Constants.TIME_DIFFERENCE_MIN){
+            int r = Constants.START_HEIGHT +
+                    ((startingT + timeDifference -
+                            Constants.TIME_DIFFERENCE_MIN - Constants.START_TIME)*Constants.HOUR_HEIGHT) -1;
+            String rContent = list.get(r);
+            list.set(r, rContent.substring(0, weekDay)+ "            " +
+                    rContent.substring(weekDay + Constants.CELL_FILLING));
+            timeDifference--;
+        }
     }
+
+    /**
+     * Reformat the list by marking a cell that is occupied by a course in the schedule.
+     *
+     * @param list the Calendar list
+     * @param weekDay weekday's location in the Calendar
+     * @param startingT starting time of this section
+     * @param courseCode course code w/ section flag, 7-char long.
+     */
+    protected void markCell(List<String> list, int weekDay, int startingT, String courseCode){
+        int rowStarting = Constants.START_HEIGHT +
+                ((startingT - Constants.START_TIME)*Constants.HOUR_HEIGHT);
+        list.set(rowStarting, list.get(rowStarting).substring(0, weekDay + 3)+
+                courseCode.substring(0, Constants.COURSE_CODE_LENGTH) +
+                list.get(rowStarting).substring(weekDay + Constants.CELL_FILLING - 2));
+        list.set(rowStarting + 1, list.get(rowStarting + 1).substring(0,weekDay + 3)+
+                courseCode.substring(Constants.COURSE_CODE_LENGTH) +
+                list.get(rowStarting + 1).substring(weekDay + Constants.CELL_FILLING -2));
+    }
+
+    /**
+     * Reformat the list by setting up the size and name of the cell based on information provided
+     *
+     * @param desiredList the Calendar list
+     * @param sections    the map representing the section's date(s) and time slots(s).
+     * @param weekday     day in the week
+     * @param courseCode  course code w/ section flag, 7-char long.
+     */
+    abstract void setupCell(List<String> desiredList, Map<String, ArrayList<String>> sections,
+                             String weekday, String courseCode) ;
+
+
 }
---
+++ src/main/java/com/courseApp/calendarService/SingledayCalendar.java
@@ -0,0 +1,63 @@
+package com.courseApp.calendarService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.courseApp.constants.Constants;
+import com.courseApp.constants.Exceptions;
+import com.courseApp.utils.TimeConversion;
+
+
+public abstract class SingledayCalendar extends CalendarFactory implements UsePresentable {
+    public SingledayCalendar(Map<String, Map<String, ArrayList<String>>> formattedSchedule) {
+        super(formattedSchedule);
+    }
+
+    /**
+     * Reformat the list by marking column that is occupied by day in the week.
+     *
+     * @param list the Calendar list
+     * @param calendarType type of the Calendar required
+     */
+    protected void markSingleDay(List<String> list, String calendarType){
+        list.set(Constants.FIRST_DAY_POSITION, list.get(Constants.FIRST_DAY_POSITION).substring(0,
+                Constants.CELL_FILLING+Constants.COURSE_CODE_FLAG)+
+                calendarType.substring(0, Constants.COURSE_WEEKDAY_FLAG) +
+                list.get(Constants.FIRST_DAY_POSITION).
+                        substring(Constants.CELL_FILLING+Constants.COURSE_CODE_FLAG+Constants.COURSE_WEEKDAY_FLAG));
+
+    }
+    /**
+     * Convert the String weekday to an integer format indicating its location in SingledayCalendar.
+     *
+     * @param weekday String of Days of week
+     * @return weekday's location in the SingledayCalendar
+     */
+    protected int getWeekDay(String weekday){
+//        if (weekday.equals(Constants.MONDAY) | weekday.equals(Constants.TUESDAY)| weekday.equals(Constants.WEDNESDAY)|
+//                weekday.equals(Constants.THURSDAY)| weekday.equals(Constants.FRIDAY)) {
+            return Constants.LOCATION_MO;
+    }
+
+    /**
+     * Reformat the list by setting up the size and name of the cell based on information provide
+     *
+     * @param desiredList the Calendar list
+     * @param sections the map representing the section's date(s) and time slots(s).
+     * @param weekday day in the week
+     * @param courseCode the 7-character String representing a course
+     */
+    @Override
+    protected void setupCell(List<String> desiredList, Map<String, ArrayList<String>> sections,
+                             String weekday, String courseCode){
+        ArrayList<String> time = sections.get(weekday);
+        int startingT = TimeConversion.getIntTime(time.get(0));
+        int endingT = TimeConversion.getIntTime(time.get(1));
+        int weekDay;
+        weekDay = getWeekDay(weekday);
+        markCell(desiredList, weekDay, startingT, courseCode);
+        resizeCell(desiredList, weekDay, startingT, TimeConversion.getTimeDiff(startingT, endingT));}
+
+
+}
